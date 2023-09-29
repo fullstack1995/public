0512803fb4b4f78412d042dcb518faccee35a27c
Joseph <70432158+fullstack1995@users.noreply.github.com>
2023-09-29 05:07:22 +0330

    Delete includes directory

--- includes/ajax-frontend.php
+++
@@ -1,9 +0,0 @@
-<?php defined( 'ABSPATH' ) or die;
-
-/* get shop products */
-add_action( 'wp_ajax_wp_get_shop_products', function() 
-{
-    $response = [];
-
-    wp_send_json_success( $response );
-} );
